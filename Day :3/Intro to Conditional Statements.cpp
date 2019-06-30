33ee7710500dce74c719e88f61924fa27a0ceb96
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2019-06-30 18:32:44 +0530

    Update Intro to Conditional Statements.cpp

--- Day :3/Intro to Conditional Statements.cpp
+++ Day :3/Intro to Conditional Statements.cpp
@@ -1,32 +1,15 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
-
-
-int main()
-{
+int main() {
     int N;
     cin >> N;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-    if(!(N%2==0)&&(N!=24))
-    {
-        cout<<"Weird";
+    if (N % 2 != 0) cout << "Weird";
+    else {
+        if (N <= 5) cout << "Not Weird";
+        else if (N <= 20) cout << "Weird";
+        else cout << "Not Weird";
     }
-    if((N%2==0)&&(2<N<5)&&(N!=24))
-    {
-        cout<<"Not Weird";
-    }
-    if((N%2==0)&&(6<N<20)&&(N!=24))
-    {
-        cout<<"Weird";
-    }
-    if(N==24)
-    {
-        cout<<"Not Weird";
-    }
-
-
-    return 0;
 }
