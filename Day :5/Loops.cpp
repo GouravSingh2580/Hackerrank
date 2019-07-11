1ffc810b40af64b52360f39bc0237c1d255cfb24
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2019-07-11 17:49:24 +0530

    Create Loops.cpp

---
+++ Day :5/Loops.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+   int n;
+   cin>>n;
+
+   for(int i=1; i<11; ++i)
+   {
+       cout<<n<<" " <<"x"<<" "<<i <<" "<<"="<<" "<<n*i<<endl;
+   }
+   return 0; 
+}
